0d38e0f183c76b2352cce09aec639a614a5c175e
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2025-02-05 21:22:17 +0700

    add cmd readme file

--- src/main/java/spring/app/blog/BlogController.java
+++ src/main/java/spring/app/blog/BlogController.java
@@ -1,4 +1,14 @@
 package spring.app.blog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BlogController {
+    @GetMapping("/blog")
+    public String blog(Model model) {
+        model.addAttribute("name", "HELLO SPRING APP");
+        return "blog";
+    }
 }
